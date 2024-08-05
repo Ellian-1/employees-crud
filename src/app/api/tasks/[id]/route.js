75219d74cd4207641eaa375b3371abe29a7f58d0
Ellian <ellianalba1@gmail.com>
2024-08-05 15:03:55 -0400

    create: create the [id] files for the rest api and it's method functions inside the route file

---
+++ src/app/api/tasks/[id]/route.js
@@ -0,0 +1,4 @@
+export function GET(request, { params }) {}
+export function POST(request, { params }) {}
+export function PUT(request, { params }) {}
+export function DELETE(request, { params }) {}
