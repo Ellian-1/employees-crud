b7936b36f5744c99a42124f9526df9fc7d87f0e7
Ellian <ellianalba1@gmail.com>
2024-08-07 09:39:38 -0400

    create: create the ui for the employees form

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import styles from "./new.module.css";
 
 const NewPage = () => {
-  return <div>NewPage</div>;
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <label htmlFor="firstName" className={styles.label}>
+          First name:
+          <input
+            id="firstName"
+            type="text"
+            className={styles.input}
+            placeholder="First name"
+          />
+        </label>
+        <label htmlFor="lastName" className={styles.label}>
+          Last name:
+          <input
+            id="lastName"
+            type="text"
+            className={styles.input}
+            placeholder="Last name"
+          />
+        </label>
+        <label htmlFor="jobDescription" className={styles.label}>
+          Job description:
+          <textarea
+            name="jobDescription"
+            id="jobDescription"
+            rows="3"
+            className={styles.input}
+            placeholder="Job description"
+          ></textarea>
+        </label>
+        <button className={styles.button}>Crear</button>
+      </form>
+    </div>
+  );
 };
 
 export default NewPage;
