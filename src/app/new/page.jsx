1462e656483e71ee9b0f432f01f223d9d1edda81
Ellian <ellianalba1@gmail.com>
2024-08-07 10:22:28 -0400

    fix: fix the NewPage component to fix the errors in the onSubmit arrow function

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,33 +1,52 @@
+"use client";
 import React from "react";
 import styles from "./new.module.css";
 
 const NewPage = () => {
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const first_name = e.target.first_name.value;
+    const last_name = e.target.last_name.value;
+    const job_description = e.target.job_description.value;
+
+    const res = await fetch("/api/employees", {
+      method: "POST",
+      body: JSON.stringify({ first_name, last_name, job_description }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <div className={styles.container}>
-      <form className={styles.form}>
-        <label htmlFor="firstName" className={styles.label}>
+      <form className={styles.form} onSubmit={onSubmit}>
+        <label htmlFor="first_name" className={styles.label}>
           First name:
           <input
-            id="firstName"
+            id="first_name"
             type="text"
             className={styles.input}
             placeholder="First name"
           />
         </label>
-        <label htmlFor="lastName" className={styles.label}>
+        <label htmlFor="last_name" className={styles.label}>
           Last name:
           <input
-            id="lastName"
+            id="last_name"
             type="text"
             className={styles.input}
             placeholder="Last name"
           />
         </label>
-        <label htmlFor="jobDescription" className={styles.label}>
+        <label htmlFor="job_description" className={styles.label}>
           Job description:
           <textarea
-            name="jobDescription"
-            id="jobDescription"
+            name="job_description"
+            id="job_description"
             rows="3"
             className={styles.input}
             placeholder="Job description"
