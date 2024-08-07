1dc32911dc01d9f930ade2c705409be8ae74358e
Ellian <ellianalba1@gmail.com>
2024-08-07 17:36:05 -0400

    update: update the Home page to render the employees info

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import styles from "@/app/Home.module.css";
+import { prisma } from "@/libs/prisma";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home = async () => {
+  const employees = await prisma.employees.findMany();
+
+  return (
+    <div className={styles.container}>
+      {employees.map((employee) => (
+        <div className={styles.employeeInfo} key={employee.id}>
+          <h3
+            className={`${styles.employeeData} ${styles.name}`}
+          >{`${employee.first_name} ${employee.last_name}:`}</h3>
+          <p
+            className={`${styles.employeeData} ${styles.job}`}
+          >{`Started work as a ${employee.job_description} at ${new Date(
+            employee.started_at
+          ).toLocaleDateString()}`}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
